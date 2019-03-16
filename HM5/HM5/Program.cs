edc63ebaaf9cb9f6054e7f0afa5071a7119c0a4e
Valujko <Valujko@gmail.com>
2019-03-16 22:55:04 +0300

    First commit

---
+++ HM5/HM5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM5
+{
+    class Program
+    {
+        static void Main(string[] args)        
+        {
+        }
+    }
+}
